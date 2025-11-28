394a1a2c4214aea9aaf17818b6063d30c87ffaac
brunomartins84 <focalicius@gmail.com>
2025-11-28 00:19:14 -0300

    Add Show collection type and Strapi schema updates

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,26 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-aws-s3',
+      providerOptions: {
+        s3Options: {
+          endpoint: env('SPACES_ENDPOINT'),
+          region: env('SPACES_REGION', 'nyc3'),
+          credentials: {
+            accessKeyId: env('SPACES_KEY'),
+            secretAccessKey: env('SPACES_SECRET'),
+          },
+        },
+        params: {
+          Bucket: env('SPACES_BUCKET'),
+        },
+        baseUrl: env('SPACES_BASE_URL'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
